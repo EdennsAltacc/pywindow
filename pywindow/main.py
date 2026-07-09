21de3b2d798fdeff359cadf081248ae730ce8e6a
notlbock <uyenla1224@gmail.com>
2026-07-08 18:05:54 -0700

    Added ctypes test

--- pywindow/main.py
+++ pywindow/main.py
@@ -1,2 +1,12 @@
+import ctypes
+import os
+
+lib_path = os.path.abspath("./so/main.so")
+lib = ctypes.CDLL(lib_path)
+
+lib.test.argtypes = [ctypes.c_void_p]
+lib.test.restype = ctypes.c_int
+
 def init():
+    print(str(lib.test()))
     print("Hello from Pywindow!")
